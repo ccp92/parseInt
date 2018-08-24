43387fd0f06127f0a802a674dbafa9681aaedf4c
Christopher Parsons <mail@christopherparsons.me>
2018-08-24 10:57:07 +0100

    Refactor tests
    Re-organised test to sit inside relevant describe block for method
    Shortened the test names as unnecessarily long
    Extracted the expect block into another method to reduce duplication

--- spec/parseint_spec.rb
+++ spec/parseint_spec.rb
@@ -4,45 +4,53 @@ require 'parseint'
 
 describe ParseInt do
   let(:parseint) { ParseInt.new }
-  context 'single digit conversions' do
-    it 'can convert one to 1' do
-      expect(parseint.parse_int('one')).to eq(1)
-    end
+  describe '.parse_int' do
+    context 'single digit conversions' do
+      it 'can convert to 1' do
+        convert('one', 1)
+      end
 
-    it 'can convert two to 2' do
-      expect(parseint.parse_int('two')).to eq(2)
-    end
+      it 'can convert to 2' do
+        convert('two', 2)
+      end
 
-    it 'can convert three into 3' do
-      expect(parseint.parse_int('three')).to eq(3)
-    end
+      it 'can convert to 3' do
+        convert('three', 3)
+      end
 
-    it 'can convert four into 4' do
-      expect(parseint.parse_int('four')).to eq(4)
-    end
+      it 'can convert to 4' do
+        convert('four', 4)
+      end
 
-    it 'can convert five into 5' do
-      expect(parseint.parse_int('five')).to eq(5)
-    end
+      it 'can convert to 5' do
+        convert('five', 5)
+      end
 
-    it 'can convert six into 6' do
-      expect(parseint.parse_int('six')).to eq(6)
-    end
+      it 'can convert to 6' do
+        convert('six', 6)
+      end
 
-    it 'can convert seven into 7' do
-      expect(parseint.parse_int('seven')).to eq(7)
-    end
+      it 'can convert to 7' do
+        convert('seven', 7)
+      end
 
-    it 'can convert eight into 8' do
-      expect(parseint.parse_int('eight')).to eq(8)
-    end
+      it 'can convert to 8' do
+        convert('eight', 8)
+      end
 
-    it 'can convert nine into 9' do
-      expect(parseint.parse_int('nine')).to eq(9)
-    end
+      it 'can convert to 9' do
+        convert('nine', 9)
+      end
 
-    it 'can convert zero into 0' do
-      expect(parseint.parse_int('zero')).to eq(0)
+      it 'can convert to 0' do
+        convert('zero', 0)
+      end
     end
+  end
+
+  private
+
+  def convert(input, output)
+    expect(parseint.parse_int(input)).to eq(output)
   end
 end
