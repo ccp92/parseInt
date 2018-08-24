a753b6d55fa5d1ed8653f4d6937702e58c2744a2
Christopher Parsons <mail@christopherparsons.me>
2018-08-24 10:28:05 +0100

    Add tests and code for all single digit conversions
    Very simple guard clauses have been added to allow sliming the tests

--- lib/parseint.rb
+++ lib/parseint.rb
@@ -2,6 +2,15 @@
 
 class ParseInt
   def parse_int(string)
-    1
+    return 1 if string == 'one'
+    return 2 if string == 'two'
+    return 3 if string == 'three'
+    return 4 if string == 'four'
+    return 5 if string == 'five'
+    return 6 if string == 'six'
+    return 7 if string == 'seven'
+    return 8 if string == 'eight'
+    return 9 if string == 'nine'
+    0
   end
 end
--- spec/parseint_spec.rb
+++ spec/parseint_spec.rb
@@ -4,7 +4,45 @@ require 'parseint'
 
 describe ParseInt do
   let(:parseint) { ParseInt.new }
-  it 'can convert one to 1' do
-    expect(parseint.parse_int('one')).to eq(1)
+  context 'single digit conversions' do
+    it 'can convert one to 1' do
+      expect(parseint.parse_int('one')).to eq(1)
+    end
+
+    it 'can convert two to 2' do
+      expect(parseint.parse_int('two')).to eq(2)
+    end
+
+    it 'can convert three into 3' do
+      expect(parseint.parse_int('three')).to eq(3)
+    end
+
+    it 'can convert four into 4' do
+      expect(parseint.parse_int('four')).to eq(4)
+    end
+
+    it 'can convert five into 5' do
+      expect(parseint.parse_int('five')).to eq(5)
+    end
+
+    it 'can convert six into 6' do
+      expect(parseint.parse_int('six')).to eq(6)
+    end
+
+    it 'can convert seven into 7' do
+      expect(parseint.parse_int('seven')).to eq(7)
+    end
+
+    it 'can convert eight into 8' do
+      expect(parseint.parse_int('eight')).to eq(8)
+    end
+
+    it 'can convert nine into 9' do
+      expect(parseint.parse_int('nine')).to eq(9)
+    end
+
+    it 'can convert zero into 0' do
+      expect(parseint.parse_int('zero')).to eq(0)
+    end
   end
 end
